b2eb17dce576f4952c592e6c72cf7a0f495f038a
Jake <jake@Jakes-MacBook-Pro.local>
2023-09-22 22:09:08 +0100

    related products

--- src/components/RelatedProductsCarousel/RelatedProductsCarousel.jsx
+++ src/components/RelatedProductsCarousel/RelatedProductsCarousel.jsx
@@ -2,12 +2,14 @@ import classes from "./RelatedProductsCarousel.module.css";
 
 function RelatedProductsCarousel(props) {
     const {name, brand, category, keywords} = props;
+
+    return null;
     
-    return(
-        <div className={classes.carousel}>
-            <h2>Related Products</h2>
-        </div>
-    );
+    // return(
+    //     <div className={classes.carousel}>
+    //         <h2>Related Products</h2>
+    //     </div>
+    // );
 }
 
 export default RelatedProductsCarousel;
